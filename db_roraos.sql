6976dc446d9cf7d63676e7ccad85e69c40bd8f8d
Robert Rozic <r.rozic97@gmail.com>
2018-01-21 18:21:23 +0100

    Update database

--- db_roraos.sql
+++ db_roraos.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 21, 2018 at 03:55 PM
+-- Generation Time: Jan 21, 2018 at 06:20 PM
 -- Server version: 5.7.20-0ubuntu0.16.04.1
--- PHP Version: 7.0.27-1+ubuntu16.04.1+deb.sury.org+1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,7 +32,7 @@ CREATE TABLE `users` (
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(12) NOT NULL,
-  `address` varchar(500) NOT NULL,
+  `address` varchar(500) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `hash` varchar(400) NOT NULL,
   `account_type` varchar(255) NOT NULL DEFAULT 'user',
@@ -44,7 +44,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `password`, `hash`, `account_type`, `status`) VALUES
-(24, 'Robert', 'Rozic', 'rozic.robert@amplius.tech', '63125327', '', '$2y$10$ulPagRvL86oULgnbkBhz.u7o9cLTzAA5p23gqyYXCd4lt3u0o1fvq', '6cfe0e6127fa25df2a0ef2ae1067d915', 'user', 1);
+(38, 'Robert', 'RoziÄ‡', 'r.rozic97@gmail.com', '63125327', NULL, '$2y$10$Hn.aEHqI9IwVKcbhI2Gs1eDvzkblT1OtYUY8N3nfJkmTnNq/nUlY.', 'd86ea612dec96096c5e0fcc8dd42ab6d', 'user', 1);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
