31858507a1cfa6105d881eae3387a4399e4c42be
Robert Rozic <r.rozic97@gmail.com>
2018-01-29 16:13:40 +0100

    Add vizija.php

---
+++ vizija.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Roraos Vizija</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+		<link href="src/css/bootstrap.min.css" rel="stylesheet">
+		<link href="src/css/fontawesome-all.min.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="src/css/navbar.css" type="text/css">
+		<link rel="stylesheet" href="src/css/main.css" type="text/css">
+	</head>
+	<body>
+		<nav id="myNavbar" class="navbar fixed-top navbar-expand-md navbar-inverse">
+			<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fa fa-2x fa-bars" aria-hidden="true"></i>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="nav navbar-nav ml-auto">
+					<li class="nav-item option"><a class="nav-link" href="index.php#home">Naslovnica</a></li>
+					<li class="nav-item option"><a class="nav-link" href="index.php#about">O nama</a></li>
+					<li class="nav-item option"><a class="nav-link" href="index.php#contact">Kontakt</a></li>
+					<li class="nav-item option"><a class="nav-link" href="">Vizija</a></li>
+					<li class="nav-item option"><a class="nav-link" href="app/index.php">Prijava</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container-fluid">
+			<section id="vizija" class="row d-flex flex-column justify-content-center align-items-center">
+				<img src="src/img/logo.png" class="img-fluid logo"/>
+				<p class="text-center">Mi Vas pokrećemo!</p>
+				<h2>Vizija</h2>
+			</section>
+			<section id="vizija-tekst" class="row d-flex flex-column justify-content-center align-items-center">
+
+			</section>
+			<p id="copyright"> 2018 Roraos.ba © All rights reserved. </p>	 
+		</div>
+
+		<script src="src/javascript/jquery-3.2.1.min.js"></script>
+		<script src="src/javascript/popper.min.js"></script>
+		<script src="src/javascript/bootstrap.min.js"></script>
+		<script src="src/javascript/index.js" type="text/javascript"></script>
+	</body>
+</html>
