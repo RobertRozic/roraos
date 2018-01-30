df1556f7d281f7da171a4de15fef219d2a6a70f1
Mario <FHawk>
2018-01-30 17:30:27 +0100

    Profil edit

--- app/profil.php
+++ app/profil.php
@@ -11,19 +11,25 @@ $html .= <<<HTML
 		<div class="container-fluid dashboard-main d-flex flex-column justify-content-center align-items-center">
 		<div class="row justify-content-center dashboard">
 			<div class="col-10 col-md-3 d-flex flex-column justify-content-center align-items-center wrapper" id="myprofile">
+				
+			<div class="d-flex flex-row justify-content-between" id="options">
 				<a href="login/logout.php">
 					<i aria-hidden="true" class="fas fa-2x fa-sign-out-alt option"></i>
 				</a>
-				<h2 class="text-center">Dobrodošli na stranicu Roraos!</h2>
+				<a href="editForm.php">
+					<i aria-hidden="true" class="fas fa-2x fa-edit option"></i>
+				</a>
+			</div>
+			<img src="../src/img/blank-profile.png" class="img-fluid">
+				<h5 class="text-center">Moj profil</h5>
 				<p class="text-center">Hvala Vam na registraciji i strpljenju.<br>
 				(Radovi na stranici su u tijeku!)</p>
 			</div>
 			<div class="col-10 col-md-7 d-flex flex-column align-items-center wrapper" id="myprofileblue">
 				<div id="myprofileblue_top"></div>
 
-				<h2 class="text-center">Dobrodošli na stranicu Roraos!</h2>
-				<p class="text-center">Hvala Vam na registraciji i strpljenju.<br>
-				(Radovi na stranici su u tijeku!)</p>
+				<br>
+				<p class="text-center">Ovdje idu neki oglasi!</p>
 			</div>
 		</div>
 	</div>
